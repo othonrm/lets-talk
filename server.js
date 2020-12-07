b45c58eee6400f70755f55009b72fa991f7c9fae
Jose Othon <othon200@gmail.com>
2020-12-07 20:24:34 -0300

    fix: sockerio server url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = require("socket.io")(server, {
         origin:
             process.env.NODE_ENV !== "production"
                 ? "*"
-                : "https://othon-lets-talk.herokuapp.com",
+                : "https://www.lets-talk.dev.br",
     },
 });
 const { ExpressPeerServer } = require("peer");
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
                   "http://127.0.0.1:8080",
                   "http://localhost:3000",
               ]
-            : ["https://othon-lets-talk.herokuapp.com"];
+            : ["https://www.lets-talk.dev.br"];
     const origin = req.headers.origin;
 
     if (allowedOrigins.includes(origin)) {
