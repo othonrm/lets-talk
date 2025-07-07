6cb693422a75ea136750c9437438c4c157ba4088
Jose Othon <othon200@gmail.com>
2025-07-07 20:12:36 -0300

    Fix api baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const baseURL = `${
-    process.env.NODE_ENV === 'development'
-        ? 'http://localhost:8080'
-        : process.env.PUBLIC_URL || process.env.APP_URL
+    process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : ''
 }/api/v1/`;
 
 const api = axios.create({
